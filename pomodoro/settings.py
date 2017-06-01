89b1d1c641ef5cc02d5f7e5c1b34df04dad816c6
Allan M <allanmukundi@hotmail.com>
2017-06-01 17:38:13 -0400

    Fixed Heroku error.

--- pomodoro/settings.py
+++ pomodoro/settings.py
@@ -95,8 +95,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'pomodoro.CustomUser'
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- users/models.py
+++ users/models.py
@@ -2,18 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser, UserManager
-
-class CustomUserManager(UserManager):
-    def get_by_natural_key(self, username):
-        case_insensitive_username_field = '{}__iexact'.format(self.model.USERNAME_FIELD)
-        return self.get(**{case_insensitive_username_field: username})
-
-class CustomUser(AbstractUser):
-    objects = CustomUserManager()
 
 class Profile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     coins = models.PositiveIntegerField(default=0)
     cycles = models.PositiveIntegerField(default=0)
 
